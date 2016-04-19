317b2ea3850d335bc886a2159b97864f305f7735
Prytkov, Sergey <SPrytkov@qulix.com>
2016-04-19 17:40:06 +0300

    update scripts

--- scripts/push_to_parent.sh
+++ scripts/push_to_parent.sh
@@ -9,22 +9,10 @@ cd ./TestParent
 mkdir ./versions
 cd ./versions
 
-#TRAVIS_COMMIT="kljhhgjhvghfrh"
-#TRAVIS_REPO_SLUG="sprytkov/TestChild"
-
 repo_name=`echo $TRAVIS_REPO_SLUG | sed 's|[^/]*/||'`
 
-ls
-
 rm -f $repo_name
-
-ls
-
 echo $TRAVIS_COMMIT >> $repo_name
-
-ls
-
-cat $repo_name
 
 git add .
 git status
